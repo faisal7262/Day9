52d7d2dfe9c1abde1d0a08b65f94315adc8ff277
faisal7262 <shaikhfaisal6342@gmail.com>
2020-10-09 00:19:04 +0530

    commit for Employee Attendance UC 8

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,27 +6,35 @@ MAX_HRS_IN_MONTH=100;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
-totalWorkHours=0;
-totalWorkingHours=0;
+totalEmpHrs=0;
+totalWorkingDays=0;
 
-function getWorkingHours(){
+function getWorkHrs() {
 	case $1 in
 		$IS_FULL_TIME)
-			workHours=8
+			empHrs=8
 			;;
+
 		$IS_PART_TIME)
-			workHours=4
+			empHrs=4
 			;;
 		*)
-			workHours=0
+			empHrs=0
 			;;
 	esac
 }
-while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+
+function getEmpWage() {
+	echo $(($1*$EMP_RATE_PER_HR))
+}
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
-	getWorkingHours $((RANDOM%3))
-	totalWorkHours=$(($totalWorkHours + $workHours))
+	empCheck=$((RANDOM%3))
+	getWorkHrs $empCheck
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	dailyWages[$totalWorkingDays]=$(($empHrs*$EMP_RATE_PER_HR))
 done
+totalSalary="$( getEmpWage $totalEmpHrs )"
 
-totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR))
