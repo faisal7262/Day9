1afa5756bfb84cdcbd2002eaf398d5be2d240d4a
faisal7262 <shaikhfaisal6342@gmail.com>
2020-10-08 23:01:22 +0530

    Commit For Employee Attendance uc 1

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
+#!/bin/bash -x
+
 echo "Welcome to Employee Wage Computation Program";
+
+abc=$((RANDOM%2));
+
+if [[ abc -eq 1 ]]
+then
+	echo "Employee Present";
+else
+	echo "Employee Absend";
+fi
+
