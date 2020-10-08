affcffd490f0b36cde87cac2644362e7884342d4
faisal7262 <shaikhfaisal6342@gmail.com>
2020-10-09 00:02:48 +0530

    commit for UC 5

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,40 +1,27 @@
 #!/bin/bash -x
 
-EMP_RATE_PER_HR=20;
-
-
-totalWorkHours=0;
-totalWorkingHours=0;
-empHrs=8;
-empPartHrs=4;
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
+numWorkingDays=20;
 
-echo "Welcome to Employee Wage Computation Program";
+for(( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$((RANDOM%3));
+		case $empCheck in
+			$isFullTime)
+				empHrs=8
+				;;
+			$isPartTime)
+				empHrs=4
+				;;
+			*)
+				empHrs=0
+				;;
+		esac
+		salary=$(($empHrs*$empRatePerHr));
+		totalSalary=$(($totalSalary+$salary));
+done
 
-Attendance=$((RANDOM%2));
-
-if [[ Attendance -eq 1 ]]
-then
-	echo "Employee Present"
-	empStatus=$((RANDOM%3));
-	case $empStatus in 
-		$isFullTime)
-			salary=$(($empHrs*$EMP_RATE_PER_HR));
-			echo "$salary"
-	;;
-		$isPartTime)
-			salary=$(($empPartHrs*$EMP_RATE_PER_HR));
-			echo "$salary"
-	;;
-
-	*)
-		echo "no salary has given";
-	;;
-	esac
-else
-	echo " Employee Absend";
-
-
-fi
 
