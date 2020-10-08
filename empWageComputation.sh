a39a7b88a600611625a6fd3859f3d4855ccb4b99
faisal7262 <shaikhfaisal6342@gmail.com>
2020-10-08 23:25:53 +0530

    Employe Attendance UC 2

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,25 @@
 #!/bin/bash -x
 
+
+EMP_RATE_PER_HR=20;
+EMP_WORKING_HR=8;
+
+totalWorkHours=0;
+totalWorkingHours=0;
+
+
 echo "Welcome to Employee Wage Computation Program";
 
 abc=$((RANDOM%2));
 
 if [[ abc -eq 1 ]]
 then
+	salary=$(($empHrs*$Emp_RATE_PER_HR));
+	echo "$salary"
 	echo " Employee Present";
 else
 	echo " Employee Absend";
+
+
 fi
 
